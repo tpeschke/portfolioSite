5d0a2cb74e07b09717a9830d3afe17b0db4871ff
Trent <mr.peschke@gmail.com>
2018-03-09 11:24:20 -0700

    gjrepw

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const express = require('express')
 
 app.use(bodyParser.json())
 app.use(cors())
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(3000, _ => {
     console.log(`All I want is our souls to touch, maybe I'll know you then 3000`)
