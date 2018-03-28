22e09719bc4bd1ad7ed7a625457bf7caa6e84fb0
Trent <mr.peschke@gmail.com>
2018-03-28 09:54:32 -0600

    debugging hosting

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(cors())
 // app.get('*', (req, res)=>{
 //     res.sendFile(path.join(__dirname, '../index.html'));
 // });
-app.use(express.static(__dirname + '../'))
+app.use(express.static(__dirname + '../index.html'))
 
 app.listen(3000, _ => {
     console.log(`All I want is our souls to touch, maybe I'll know you then 3000`)
